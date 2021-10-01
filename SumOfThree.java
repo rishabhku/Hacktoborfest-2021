7a05d311b10a92a841b15ad1466f7bcc41256e6a
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-10-01 23:08:18 +0530

    Sum

---
+++ SumOfThree.java
@@ -0,0 +1,27 @@
+/* Author : Chaitaly Kundu
+
+   Date : 01-09-2021*/
+
+   public class SumOfThree{
+
+        static int addition(int a, int b, int c)
+
+        {
+
+            int res = a+b+c;
+
+            return res;
+
+        }
+
+       public static void main(String[] args) {
+
+           int a=10 ,b=15 ,c=20;
+
+           int res = addition(a,b,c);
+
+           System.out.println(res);
+
+       }
+
+   }
